9c2be0cf5365b22185a0ed26b2dfe398ebee12bb
samvrny <samvrny@gmail.com>
2022-08-05 14:31:51 -0500

    got initial fetch/get request running and connected to db.json

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    //console.log(results);
+    res.json(results);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on ${PORT}!`);
+});
