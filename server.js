338657a9b909054c9e0dee80881dd36bc73d9740
samvrny <samvrny@gmail.com>
2022-08-05 14:43:19 -0500

    got html printing to page with a get request

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
-    //console.log(results);
     res.json(results);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.listen(PORT, () => {
