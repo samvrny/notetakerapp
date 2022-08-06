05b70d365b1d772dbf1755185972519a5f4568f1
samvrny <samvrny@gmail.com>
2022-08-06 11:02:26 -0500

    added office hours example

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ function createNewNote(body, notesArray) {
     notesArray.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify({notes: notesArray}, null) //NOTE: If something breaks, there used to be a comma here at the end of null ( null,)
+        JSON.stringify({notes: notesArray}, null, 2) //NOTE: If something breaks, there used to be a comma here at the end of null ( null,)
     );
     return newNote;
 }
@@ -40,5 +40,33 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on ${PORT}!`);
+    console.log(`API server now on PORT ${PORT}!`);
 });
+
+
+
+//EXAMPLE FOR OFFICE HOURS BELOW
+
+function apple (arg1, arg2) {
+    console.log(arg1, arg2);
+}
+
+function appleEx() {
+    let blue = 'blue';
+    let yellow = 'yellow';
+apple(blue, yellow);
+}
+
+function appleExTwo() {
+    let red = 'red';
+    let orange = 'orange';
+    apple(red, orange);
+}
+
+appleEx();
+appleExTwo();
+
+//Basically, if set up a function with two parameters (apple in this instance) 
+//Then whenever I call that function with arguments will they always get set into
+//the value or the parameters? Basically are the parameters just waiting to accept 
+//data sent (arguments?)
